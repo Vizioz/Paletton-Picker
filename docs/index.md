6de2e9e9d0fec2fa5fab0eb1a639dc4c9048c7b5
Miguel Lopez <mlopez@vizioz.com>
2020-02-18 16:27:13 +0000

    working example

--- docs/index.md
+++ docs/index.md
@@ -1,14 +1,24 @@
 ---
 layout: default
 title: Overview
+styles: example
+scripts: example
 ---
 
-## Palette Picker. A customizable colour template data type for Umbraco.
+<h2 class="complement-4 border-complement-2">Palette Picker. A customizable colour template data type for Umbraco.</h2>
 
 Palette Picker allows editors to select a predefined coloer template for any particular page of their site.
 
 This can be especially useful for document types where we want colours to change often or be customizable.
 
-It makes it really simple for editors to change the colour template value, while it requests only a minimum configuration from developers to set up the available colour templates. It will generate a series of CSS rules that can imported into any view, and then use the specific classes accordingly on any colour-custom element of the view. The CSS rules included contain the properties color, background-color and border-color for main classes, as well as for other optional pseudo-elements and pseudo-classes. For more info, please refer to the [reference section]({{ site.baseurl }}/documentation#reference-css) of the documentation page.
+It makes it really simple for editors to change the colour template value, while it requests only a minimum configuration from developers to set up the available colour templates. It will generate a series of CSS rules that can imported into any view, and then use the specific classes accordingly on any colour-custom element of the view. The CSS rules included contain the properties color, background-color and border-color for main classes, as well as for other optional pseudo-elements and pseudo-classes. For more info, please refer to the <a href="{{ site.baseurl }}/documentation#reference-css" class="primary-0 hover-primary-0">Reference section</a> of the documentation page.
 
-The colour templates are based on [Paletton.com](https://paletton.com/). The templates can be configured and imported from there into the Palette Picker data type. The current version of Palette Picker only accepts the colour information imported as XML.
+The colour templates are based on <a href="https://paletton.com/" class="primary-0 hover-primary-0">Paletton.com</a>. The templates can be configured and imported from there into the Palette Picker data type. The current version of Palette Picker only accepts the colour information imported as XML.
+
+<h2 class="complement-4 border-complement-2">Working Example</h2>
+
+Here you can check an example of how Palette Picker will work on your site.
+
+Below is a snippet replicating a property of a content node in the Umbraco back office. Simply change the value of the colour template and see why your editors will love it.
+
+{% include example.html %}
---
+++ docs/js/example.js
@@ -0,0 +1,148 @@
+$(document).ready(function(){
+
+  var palette = [["4183C4", "a7c1db", "7eb0e1", "234261", "052748"], ["eee", "666", "999", "333", "222"]];
+
+  function submitColor(id) {
+    var item = $(".palette-item[data-item-id='" + id + "']");
+
+    palette[0][0] = item.find(".color-block[data-id='primary-0']").data("rgb");
+    palette[0][1] = item.find(".color-block[data-id='primary-1']").data("rgb");
+    palette[0][2] = item.find(".color-block[data-id='primary-2']").data("rgb");
+    palette[0][3] = item.find(".color-block[data-id='primary-3']").data("rgb");
+    palette[0][4] = item.find(".color-block[data-id='primary-4']").data("rgb");
+    palette[1][0] = item.find(".color-block[data-id='complement-0']").data("rgb");
+    palette[1][1] = item.find(".color-block[data-id='complement-1']").data("rgb");
+    palette[1][2] = item.find(".color-block[data-id='complement-2']").data("rgb");
+    palette[1][3] = item.find(".color-block[data-id='complement-3']").data("rgb");
+    palette[1][4] = item.find(".color-block[data-id='complement-4']").data("rgb");
+
+    $("#editor-picker .add-text").hide();
+    $("#editor-picker .palette-preview").show();
+    $(".remove-text").show();
+
+    preview();
+  };
+
+  function resetColor(){
+    palette = [["4183C4", "a7c1db", "7eb0e1", "234261", "052748"], ["666", "eee", "999", "333", "222"]];
+
+    $("#editor-picker .add-text").show();
+    $("#editor-picker .palette-preview").hide();
+    $(".remove-text").hide();
+
+    preview();
+  }
+
+  function preview() {
+    $("#editor-picker .palette-preview .color-block[data-id='primary-0'] .color-block-color").css("background-color", "#" + palette[0][0]);
+    $("#editor-picker .palette-preview .color-block[data-id='primary-1'] .color-block-color").css("background-color", "#" + palette[0][1]);
+    $("#editor-picker .palette-preview .color-block[data-id='primary-2'] .color-block-color").css("background-color", "#" + palette[0][2]);
+    $("#editor-picker .palette-preview .color-block[data-id='primary-3'] .color-block-color").css("background-color", "#" + palette[0][3]);
+    $("#editor-picker .palette-preview .color-block[data-id='primary-4'] .color-block-color").css("background-color", "#" + palette[0][4]);
+    $("#editor-picker .palette-preview .color-block[data-id='complement-0'] .color-block-color").css("background-color", "#" + palette[1][0]);
+    $("#editor-picker .palette-preview .color-block[data-id='complement-1'] .color-block-color").css("background-color", "#" + palette[1][1]);
+    $("#editor-picker .palette-preview .color-block[data-id='complement-2'] .color-block-color").css("background-color", "#" + palette[1][2]);
+    $("#editor-picker .palette-preview .color-block[data-id='complement-3'] .color-block-color").css("background-color", "#" + palette[1][3]);
+    $("#editor-picker .palette-preview .color-block[data-id='complement-4'] .color-block-color").css("background-color", "#" + palette[1][4]);
+  }
+
+  function getSheet() {
+    $("#style-palette").remove();
+
+    var style = document.createElement("style");
+    style.setAttribute("id", "style-palette");
+    style.appendChild(document.createTextNode(""));
+    document.head.appendChild(style);
+    return style.sheet;
+  };
+
+  function addCSSRule(sheet, selector, rules, index) {
+    if("insertRule" in sheet) {
+      sheet.insertRule(selector + "{" + rules + "}", index);
+    }
+    else if("addRule" in sheet) {
+      sheet.addRule(selector, rules, index);
+    }
+  }
+
+  function applyStyles() {
+    var sheet = getSheet();
+    addCSSRule(sheet, ".primary-0", "color: #" + palette[0][0] + " !important;");
+    addCSSRule(sheet, ".bg-primary-0", "background-color: #" + palette[0][0] + " !important;");
+    addCSSRule(sheet, ".border-primary-0", "border-color: #" + palette[0][0] + " !important;");
+    addCSSRule(sheet, ".hover-primary-0:hover", "color: #" + palette[0][0] + " !important;");
+    addCSSRule(sheet, ".primary-1", "color: #" + palette[0][1] + " !important;");
+    addCSSRule(sheet, ".bg-primary-1", "background-color: #" + palette[0][1] + " !important;");
+    addCSSRule(sheet, ".border-primary-1", "border-color: #" + palette[0][1] + " !important;");
+    addCSSRule(sheet, ".hover-primary-1:hover", "color: #" + palette[0][1] + " !important;");
+    addCSSRule(sheet, ".primary-2", "color: #" + palette[0][2] + " !important;");
+    addCSSRule(sheet, ".bg-primary-2", "background-color: #" + palette[0][2] + " !important;");
+    addCSSRule(sheet, ".border-primary-2", "border-color: #" + palette[0][2] + " !important;");
+    addCSSRule(sheet, ".hover-primary-2:hover", "color: #" + palette[0][2] + " !important;");
+    addCSSRule(sheet, ".primary-3", "color: #" + palette[0][3] + " !important;");
+    addCSSRule(sheet, ".bg-primary-3", "background-color: #" + palette[0][3] + " !important;");
+    addCSSRule(sheet, ".border-primary-3", "border-color: #" + palette[0][3] + " !important;");
+    addCSSRule(sheet, ".hover-primary-3:hover", "color: #" + palette[0][3] + " !important;");
+    addCSSRule(sheet, ".primary-4", "color: #" + palette[0][4] + " !important;");
+    addCSSRule(sheet, ".bg-primary-4", "background-color: #" + palette[0][4] + " !important;");
+    addCSSRule(sheet, ".border-primary-4", "border-color: #" + palette[0][4] + " !important;");
+    addCSSRule(sheet, ".hover-primary-4:hover", "color: #" + palette[0][4] + " !important;");
+    addCSSRule(sheet, ".complement-0", "color: #" + palette[1][0] + " !important;");
+    addCSSRule(sheet, ".bg-complement-0", "background-color: #" + palette[1][0] + " !important;");
+    addCSSRule(sheet, ".border-complement-0", "border-color: #" + palette[1][0] + " !important;");
+    addCSSRule(sheet, ".hover-complement-0:hover", "color: #" + palette[1][0] + " !important;");
+    addCSSRule(sheet, ".complement-1", "color: #" + palette[1][1] + " !important;");
+    addCSSRule(sheet, ".bg-complement-1", "background-color: #" + palette[1][1] + " !important;");
+    addCSSRule(sheet, ".border-complement-1", "border-color: #" + palette[1][1] + " !important;");
+    addCSSRule(sheet, ".hover-complement-1:hover", "color: #" + palette[1][1] + " !important;");
+    addCSSRule(sheet, ".complement-2", "color: #" + palette[1][2] + " !important;");
+    addCSSRule(sheet, ".bg-complement-2", "background-color: #" + palette[1][2] + " !important;");
+    addCSSRule(sheet, ".border-complement-2", "border-color: #" + palette[1][2] + " !important;");
+    addCSSRule(sheet, ".hover-complement-2:hover", "color: #" + palette[1][2] + " !important;");
+    addCSSRule(sheet, ".complement-3", "color: #" + palette[1][3] + " !important;");
+    addCSSRule(sheet, ".bg-complement-3", "background-color: #" + palette[1][3] + " !important;");
+    addCSSRule(sheet, ".border-complement-3", "border-color: #" + palette[1][3] + " !important;");
+    addCSSRule(sheet, ".hover-complement-3:hover", "color: #" + palette[1][3] + " !important;");
+    addCSSRule(sheet, ".complement-4", "color: #" + palette[1][4] + " !important;");
+    addCSSRule(sheet, ".bg-complement-4", "background-color: #" + palette[1][4] + " !important;");
+    addCSSRule(sheet, ".border-complement-4", "border-color: #" + palette[1][4] + " !important;");
+    addCSSRule(sheet, ".hover-complement-4:hover", "color: #" + palette[1][4] + " !important;");
+  }
+
+  vm = {
+      edit: function() {
+        $(".umb-editor__overlay").show();
+        $(".umb-editors").css("visibility", "visible");
+        $(".umb-editors").css("opacity", "1");
+        $(".umb-editors .umb-editor").css("transform", "translateX(0)");
+      },
+      remove: function(){
+        resetColor();
+      },
+      close: function() {
+        $(".umb-editor__overlay").hide();
+        $(".umb-editors .umb-editor").css("transform", "translateX(500px)");
+        setTimeout(function(){
+            $(".umb-editors").css("visibility", "hidden");
+            $(".umb-editors").css("opacity", "0");
+          }, 300);
+      },
+      select: function(el) {
+        $(".palette-item").removeClass("selected");
+        $(el).addClass("selected");
+        itemId = $(el).data("item-id");
+      },
+      submit: function() {
+        $(".umb-editor__overlay").hide();
+        $(".umb-editors .umb-editor").css("transform", "translateX(500px)");
+        setTimeout(function(){
+            $(".umb-editors").css("visibility", "hidden");
+            $(".umb-editors").css("opacity", "0");
+          }, 300);
+        submitColor(itemId);
+      },
+      savePublish: function() {
+        applyStyles();
+      }
+    };
+});
